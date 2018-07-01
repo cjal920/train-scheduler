2cd85aa981e05bc117f44bb3579845ab76329f7f
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-06-30 22:56:43 -0400

    final code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var config = {
   storageBucket: "trainone-3ef48.appspot.com",
   messagingSenderId: "359069671225"
 };
-firebase.initializeApp(config);
+firebase.initializeApp(config); 
 
 var database = firebase.database(); 
 
@@ -32,45 +32,23 @@ $("#add-train").on("click", function (event) {
   })
 })
 
-database.ref().on("child_added", function (childSnapshot) {
-
-  // Use Moments.js to calculate "Next Arrival" when next train will arrive, relative to current time. 
-  // Use Moments.js to calculate "Minutes Away" when next train will arrive in minutes
-
-  // Find when next train will arrive
+database.ref().on("child_added", function(childSnapshot) {
   var tFrequency = childSnapshot.val().frequency;
-
-  // Push start time back one year to make sure it comes before the current time
   var convertedDate = moment(childSnapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");
-  var trainTime = moment(convertedDate).format('hh:mm');
-
-  // Get current Time
-  var currentTime = moment();
-
-  // Push back one year to make sure it comes before current time
-  var firstTimeConverted = moment(trainTime, 'hh:mm').subtract(1, 'years');
-
-  // Calculate difference between train start time and current time
+  var trainTime = moment(convertedDate).format("hh:mm");
+  var firstTimeConverted = moment(trainTime, "hh:mm").subtract(1, "years");
   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
-  // Calculate time apart (remainder)
   var tRemainder = diffTime % tFrequency;
-
-  // Calculate minutes until train arrival
   var tMinutesTillTrain = tFrequency - tRemainder;
+  var nextTrainArrival = moment().add(tMinutesTillTrain, "m").format("hh:mm A"); 
+  $("#trainTable").append("<tr><td>" + childSnapshot.val().trainName + "</td><td>" + childSnapshot.val().destination + "</td><td>" + childSnapshot.val().frequency + "</td><td>" + nextTrainArrival + "</td><td>" + tMinutesTillTrain + "</td></td>");
 
-  // Calculate next train adding mins till current time and format - calc mins before arrival
-  // Add two more table data next arrival and mins till next train
-  var nextTrain = moment().add(tMinutesTillTrain, "minutes").format('hh:mm');
-
-  // Display Current Train Schedule
-  $("#trainTable").append(
-      "<tr><td>" + childSnapshot.val().trainName + "</td><td>" + childSnapshot.val().destination + "</td><td>"
-      + childSnapshot.val().frequency + "</td><td>" + trainTime + "</td><td>" + tMinutesTillTrain + "</td></tr>")
-}, function (errorObject) {
-  console.log("Errors handled:" + errorObject.code);
-})
+  console.log(moment());
+}); 
 
 setInterval(function () {
   location.reload();
-}, 60000)
+}, 60000);
+
+
+
